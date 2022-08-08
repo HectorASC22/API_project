723c4e1841acc16f1bce02e833d5fc3aab86ea38
HectorASC22 <hamaya409@gmail.com>
2022-08-08 16:47:21 -0400

    added button

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 let requestURL = "https://api.imgflip.com/get_memes";
 
 function displayMeme(data) {
-    let memeObject = data.data.memes[randInt(data.data.memes.length)];
+    fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+
+    .then(function (data) {
+        let memeObject = data.data.memes[randInt(data.data.memes.length)];
 
     let imgContainer = document.querySelector("img");
     imgContainer.src = memeObject.url;
 
     let caption = document.querySelector("p");
     caption.innerHTML = memeObject.name; 
+    })
+
+    .catch(function (error) {
+        console.log("Error during fetch:", error);
+    });
+
 }
 
 function randInt(upperLimit) {
     return Math.floor(Math.random()*upperLimit);
 }
 
-fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
 
-    .then(displayMeme)
 
-    .catch(function (error) {
-        console.log("Error during fetch:", error);
-    });
+
+
+let button = document.querySelector("button");
+button.onclick = displayMeme;
+
+
+
+
